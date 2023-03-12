5f8e377758d2163bb73c3d41c057ba17388f85bf
MariahCasciola417 <117303840+MariahCasciola@users.noreply.github.com>
2023-03-12 18:54:40 -0500

    added cors for authentication

--- src/app.js
+++ src/app.js
@@ -1,15 +1,18 @@
 if (process.env.USER) require("dotenv").config();
 const express = require("express");
 const moviesRouter = require("./movies/movies.router");
-const theatersRouter = require("./theaters/theaters.router")
-const reviewsRouter = require("./reviews/reviews.router")
+const theatersRouter = require("./theaters/theaters.router");
+const reviewsRouter = require("./reviews/reviews.router");
 const app = express();
+const cors = require("cors");
 
 app.use(express.json());
+// added for backend authentication for the preflight front end
+app.use(cors());
 
 app.use("/movies", moviesRouter);
-app.use("/theaters", theatersRouter)
-app.use("/reviews", reviewsRouter)
+app.use("/theaters", theatersRouter);
+app.use("/reviews", reviewsRouter);
 
 // Not Found Handler
 app.use((req, res, next) => {
